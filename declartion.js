154bc4a75d56cd022ccf5ae5648e8d5239abb982
chinniliki <likichinni123@gmail.com>
2024-12-24 09:45:53 +0530

    Added file

---
+++ declartion.js
@@ -0,0 +1,15 @@
+function add(a,b) {
+    console.log(a+b);
+}
+add(100,200);
+const sub = function(a,b) {
+    console.log(a-b)
+}
+sub(200,100);
+const mul = (a,b)=>{
+    console.log(a*b)
+}
+mul(200,300);
+const msg = ()=>"Hello World";
+let res = msg();
+console.log(res);
